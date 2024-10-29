a0f72403a9e7d50ae30ed7eda2e153d985a92628
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-10-30 00:40:22 +0900

    install express and run the server

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express"); // import and assign express framework
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  // run the server on assigned port
+  console.log(`Example app is listening on port ${port}`);
+});
