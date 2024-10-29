72fb581f6189b131592b22dcad6a85f7cc7e8674
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-10-30 03:12:48 +0900

    create dot env to decode dbinfo

--- server/handler/api.js
+++ server/handler/api.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
 const db_info = {
-  host: "database-1.c9mcyqkkco2j.us-east-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "blogpasswordjhhshb",
-  database: "blogdemo",
-  charset: "utf8mb4", // 인코딩 설정 추가
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  charset: process.env.DB_CHARSET,
 };
 
 const connection = mysql.createConnection(db_info);
