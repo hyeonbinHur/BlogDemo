acecda72f478ec1d6064c3236f3b8a315b32c653
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-10-31 18:44:31 +0900

    create new post from frontend done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import Router from "./Router";
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Feed from './pages/Feed';
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <Router />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/api/post_api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const base = 'http://localhost:3000/test/api/post';
+
+const createPost = async (newPost) => {
+  try {
+    const response = await axios.post(base, newPost);
+
+    return response.data;
+  } catch (err) {
+    console.error('Error creating post:', err);
+    throw err;
+  }
+};
+
+const readPosts = async () => {
+  try {
+    const response = await axios.get(base);
+    return response.data; // response가 아닌 response.data를 반환
+  } catch (err) {
+    console.error('Error reading posts:', err);
+    throw err;
+  }
+};
+
+export { createPost, readPosts };
