fd6b2b1099aa4310a88915eada2ded4bf3b4a3f6
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-11-01 23:53:58 +0900

    completed to resize the image

--- client/src/pages/EditorTest.jsx
+++ client/src/pages/EditorTest.jsx
@@ -3,9 +3,7 @@ import QuillTest from '../components/texteditor/QuillTest';
 export default function EditorTest() {
   return (
     <div>
-      <div>
-        <Tui />
-      </div>
+      <div>{/* <Tui /> */}</div>
       <div>
         <QuillTest />
       </div>
