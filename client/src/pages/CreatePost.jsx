822d48bf7f80d97971f239167dd660981572f34e
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-11-01 15:16:44 +0900

    cheditor test

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -7,6 +7,7 @@ import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
 
 ///TUI test -> not support image resizing functionalities, might be unable to customize codeblocks with specific language.
+
 // import '@toast-ui/editor/dist/toastui-editor.css';
 // import { Editor } from '@toast-ui/react-editor';
 // // import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
@@ -16,6 +17,20 @@ import Stack from 'react-bootstrap/Stack';
 // import Prism from 'prismjs';
 // import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 // 또는 다른 테마
+
+//CKEditor
+
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import {
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Italic,
+  Mention,
+  Paragraph,
+  Undo,
+} from 'ckeditor5';
+import 'ckeditor5/ckeditor5.css';
 
 export default function CreatePost() {
   const queryClient = useQueryClient();
@@ -126,6 +141,21 @@ export default function CreatePost() {
         useCommandShortcut={true}
         plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
       /> */}
+
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          toolbar: {
+            items: ['undo', 'redo', '|', 'bold', 'italic'],
+          },
+          plugins: [Bold, Essentials, Italic, Mention, Paragraph, Undo],
+          licenseKey: '<YOUR_LICENSE_KEY>',
+          mention: {
+            // Mention configuration
+          },
+          initialData: '<p>Hello from CKEditor 5 in React!</p>',
+        }}
+      />
     </div>
   );
 }
