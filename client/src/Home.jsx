68d42ab2c02f1ef57d70c29531ea76fa10c7263a
RBGonion <yopi27@naver.com>
2024-11-01 15:32:47 +0900

    pull전 변경사항 저장

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,26 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
 
 const Home = () => {
-  const [data, setData] = useState([]);
-  function clickBtn() {
+  function clickBtn1() {
     axios.get("http://localhost:3000/test/api/tests").then((response) => {
-      console.log(response.data); // 응답 데이터 출력
+      console.log(response.data[0]); // 응답 데이터 출력
+    });
+  }
+
+  function clickBtn2() {
+    axios
+      .get("http://localhost:3000/test/api/post")
+      .then((response) => {
+        console.log(response.data); // response.data로 실제 데이터에 접근
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+
+  function clickBtn3() {
+    axios.get("http://localhost:3000/test/api/tests/:1").then((response) => {
+      console.log(response); // 응답 데이터 출력
     });
   }
 
@@ -13,7 +28,13 @@ const Home = () => {
     <>
       <div className="main-container">
         <div className="sub-container">
-          <button className="communicationBtn" onClick={clickBtn}>
+          <button className="communicationBtn1" onClick={clickBtn1}>
+            버튼
+          </button>
+          <button className="communicationBtn2" onClick={clickBtn2}>
+            버튼
+          </button>
+          <button className="communicationBtn3" onClick={clickBtn3}>
             버튼
           </button>
           <div>Hello world</div>
